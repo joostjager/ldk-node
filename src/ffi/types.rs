4e2662e0bbbf82641422417becd3807c90ec98ab
Elias Rohrer <dev@tnull.de>
2025-05-21 14:37:38 +0200

    f Account for changes in BOLT11 interface

--- src/ffi/types.rs
+++ src/ffi/types.rs
@@ -20,13 +20,14 @@ pub use crate::logger::{LogLevel, LogRecord, LogWriter};
 pub use crate::payment::store::{
 	ConfirmationStatus, LSPFeeLimits, PaymentDirection, PaymentKind, PaymentStatus,
 };
-pub use crate::payment::{MaxTotalRoutingFeeLimit, QrPaymentResult, SendingParameters};
+pub use crate::payment::QrPaymentResult;
 
 pub use lightning::chain::channelmonitor::BalanceSource;
 pub use lightning::events::{ClosureReason, PaymentFailureReason};
 pub use lightning::ln::types::ChannelId;
 pub use lightning::offers::offer::OfferId;
 pub use lightning::routing::gossip::{NodeAlias, NodeId, RoutingFees};
+pub use lightning::routing::router::RouteParametersConfig;
 pub use lightning::util::string::UntrustedString;
 
 pub use lightning_types::payment::{PaymentHash, PaymentPreimage, PaymentSecret};
